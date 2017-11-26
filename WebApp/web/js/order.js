3ab3923e39ccee4b89f1e49176b56fb42c5ab9fd
KevinLiem <kevin18.liem@gmail.com>
2017-11-27 00:19:36 +0700

    Add functionality to call service

--- WebApp/web/js/order.js
+++ WebApp/web/js/order.js
@@ -1,5 +1,15 @@
-function choose(idDriver, username){
+function choose(idDriver, username, customerName){
     document.getElementById('chosen-driver').value = idDriver;
     document.getElementById('driver-username').value = username;
-
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if(this.readyState == 4 && this.status == 200){
+            var text = xhttp.responseText;
+            document.cookie = "userDriver="+username;
+        }
+    };
+    xhttp.open("POST", "http://localhost:3000/notifyDriver", true);
+    xhttp.setRequestHeader("content-type","application/x-www-form-urlencoded");
+    var params = "name="+username+"&token="+customerName;
+    xhttp.send(params);
 }
